030c318c48979a10cad276e72c8ff1888df23ef7
alstdu <lexi.dugo@gmail.com>
2024-11-10 00:38:15 -0800

    feat: Render the app using React

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders quiz title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/What Kind of Tea Are You\?/i);
+  expect(titleElement).toBeInTheDocument();
 });
